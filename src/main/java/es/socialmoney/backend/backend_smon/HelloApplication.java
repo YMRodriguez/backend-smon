beca469b4274a0f30a5e86891ebd87083eb5caf5
jramosf99 <jramosf99@gmail.com>
2021-04-04 19:29:55 +0200

    proyecto en eclipse

--- src/main/java/es/socialmoney/backend/backend_smon/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package es.socialmoney.backend.backend_smon;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
