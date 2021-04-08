bffefeea49c2573f776a60a7aae988f72e8f1ca8
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-04-08 17:41:54 +0200

    Add rest

---
+++ src/main/java/es/socialmoney/rest/App.java
@@ -0,0 +1,17 @@
+package es.socialmoney.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import es.socialmoney.rest.Resources;
+
+@ApplicationPath("rest")
+
+public class App extends ResourceConfig {
+
+        public App() {
+                packages("es.socialmoney.rest");
+        }
+
+}
