feb1b0b3bc1cb3ba050128e6d9ef7a6cc434ece6
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-04-15 06:08:50 +0200

    EditAccountsServlet not fully working, does not save things in the database

--- SMON-SERVICE/src/main/java/es/socialmoney/servlets/EditAccountServlet.java
+++ SMON-SERVICE/src/main/java/es/socialmoney/servlets/EditAccountServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.IOUtils;
 
 import es.socialmoney.dao.AccountDAOImplementation;
 import es.socialmoney.model.Account;
@@ -16,6 +19,8 @@ import es.socialmoney.model.Account;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringReader;
 
 @WebServlet("/editprofile")
@@ -27,38 +32,59 @@ public class EditAccountServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		System.out.println(req.getParameter("dato"));
-		/*
-		 * StringBuilder buffer = new StringBuilder(); BufferedReader reader =
-		 * req.getReader(); String line; while ((line = reader.readLine()) != null) {
-		 * buffer.append(line); } String data = buffer.toString(); JsonReader jsonReader
-		 * = Json.createReader(new StringReader(data)); JsonObject jsonObject =
-		 * jsonReader.readObject();
-		 * 
-		 * String pricture = jsonObject.getString("picture");
-		 * 
-		 * String description = jsonObject.getString("description");
-		 * 
-		 * String password = jsonObject.getString("password");
-		 * 
-		 * boolean profits = Boolean.valueOf(jsonObject.getString("showprofits"));
-		 * 
-		 * Account account =
-		 * AccountDAOImplementation.getInstance().read(jsonObject.getString("username"))
-		 * ;
-		 * 
-		 * account.setShowprofits(profits);
-		 * 
-		 * if(password != null) { account.setPassword(password); } if(description !=
-		 * null) { account.setDescription(description); }
-		 * 
-		 * if(picture) {account.setPicture(picture);}
-		 * 
-		 * jsonObject = Json.createObjectBuilder() .add("code",200) .build();
-		 * resp.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-		 * resp.setContentType("application/json"); resp.setCharacterEncoding("UTF-8");
-		 * resp.getWriter().write(jsonObject.toString());
-		 */
+		// String data = buffer.toString();
+		JsonReader jsonReader = Json.createReader(new StringReader(getValue(req.getPart("data"))));
+		JsonObject jsonObject = jsonReader.readObject();
+		System.out.println(jsonObject);
+
+		InputStream filecontent = req.getPart("picture").getInputStream();
+		byte[] fileAsByteArray = IOUtils.toByteArray(filecontent);
+		
+		String description = jsonObject.getString("description");
+		System.out.println(description);
+
+		String password = jsonObject.getString("password");
+
+		boolean profits = jsonObject.getBoolean("showprofits");
+		
+
+		Account account = AccountDAOImplementation.getInstance().read(jsonObject.getString("username"));
+
+		account.setShowprofits(profits);
+
+		if (password != null) {
+			account.setPassword(password);
+		}
+		if (description != null) {
+			account.setDescription(description);
+		}
+
+		if (fileAsByteArray.length == 0) {
+			account.setPicture(fileAsByteArray);
+		}
+
+        Account editedAccount = AccountDAOImplementation.getInstance().read(jsonObject.getString("username"));
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		resp.setContentType("application/json");
+		resp.setCharacterEncoding("UTF-8");
+		ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(account);
+        jsonObject = Json.createObjectBuilder()
+                    .add("code",200)
+                    .add("account",json)
+                    .build();
+        resp.getWriter().write(jsonObject.toString());
+        req.getSession().setAttribute("account", editedAccount);
+	}
+
+	private static String getValue(Part part) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(part.getInputStream(), "UTF-8"));
+		StringBuilder value = new StringBuilder();
+		char[] buffer = new char[1024];
+		for (int length = 0; (length = reader.read(buffer)) > 0;) {
+			value.append(buffer, 0, length);
+		}
+		return value.toString();
 	}
 
 }
