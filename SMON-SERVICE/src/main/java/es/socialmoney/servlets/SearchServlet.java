f2c6b64eb47f2a0df51a513355201c922c514220
Diego <diego.garcia.fierro@alumnos.upm.es>
2021-04-15 11:08:30 +0200

    <SearchServlet implemented and tested. PublicationsServlet and FollowServlet implemented but not tested yet.>

---
+++ SMON-SERVICE/src/main/java/es/socialmoney/servlets/SearchServlet.java
@@ -0,0 +1,66 @@
+package es.socialmoney.servlets;
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import es.socialmoney.dao.AccountDAOImplementation;
+import es.socialmoney.model.Account;
+import es.socialmoney.model.Post;
+
+
+/**
+ *Implementation of PublicationsServlet class 
+ */
+@WebServlet("/search")
+public class SearchServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:3000"); 	
+		StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        String data = buffer.toString();
+        JsonReader jsonReader = Json.createReader(new StringReader(data));
+        JsonObject jsonObject = jsonReader.readObject();
+        
+		String username = req.getParameter(jsonObject.getString("username"));
+		Account account = AccountDAOImplementation.getInstance().read(username);
+		
+		if (account != null) {
+			ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(account);
+			 jsonObject = Json.createObjectBuilder()
+                       .add("code",200)
+                       .add("account", json)
+                       .build();
+           resp.setContentType("application/json");
+           resp.setCharacterEncoding("UTF-8");	
+           resp.getWriter().write(jsonObject.toString());
+		}
+		else {
+			 jsonObject = Json.createObjectBuilder()
+                   .add("code",204)
+                   .build();
+		}        
+		
+	}
+	
+}
